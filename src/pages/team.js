7a4836c2d586bb5caf43841c4c439125202764a4
Talgat Manglayev <talgatmanglayev@MacBook-Pro-Talgat.local>
2023-11-30 02:09:54 +0600

    edited the team page

--- src/pages/team.js
+++ src/pages/team.js
@@ -12,6 +12,20 @@ function Team()
         <Layout>
         <h1>Meet the Nazarbayev University RF Research Team</h1>
         <div className="row">
+            <div className="column" style={{display: "block", marginLeft: "auto", marginRight: "auto",}}>
+                <div className="card">
+                <img src={img3} alt="John" className="photo" />
+                <div className="container">
+                    <h2>John Doe</h2>
+                    <p className="title">Designer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
             <div className="column">
                 <div className="card">
                 <img src={img1} alt="Jane" className="photo" />
@@ -50,7 +64,127 @@ function Team()
                 </div>
                 </div>
             </div>
-        </div>    
+        </div>
+        <div className="row">
+            <div className="column">
+                <div className="card">
+                <img src={img1} alt="Jane" className="photo" />
+                <div className="container">
+                    <h2>Jane Doe</h2>
+                    <p className="title">CEO &amp; Founder</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                <img src={img2} alt="Mike" className="photo" />
+                <div className="container">
+                    <h2>Mike Ross</h2>
+                    <p className="title">Art Director</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                <img src={img3} alt="John" className="photo" />
+                <div className="container">
+                    <h2>John Doe</h2>
+                    <p className="title">Designer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="column">
+                <div className="card">
+                <img src={img1} alt="Jane" className="photo" />
+                <div className="container">
+                    <h2>Jane Doe</h2>
+                    <p className="title">CEO &amp; Founder</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                <img src={img2} alt="Mike" className="photo" />
+                <div className="container">
+                    <h2>Mike Ross</h2>
+                    <p className="title">Art Director</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                <img src={img3} alt="John" className="photo" />
+                <div className="container">
+                    <h2>John Doe</h2>
+                    <p className="title">Designer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+        </div> 
+        <div className="row">
+            <div className="column">
+                <div className="card">
+                <img src={img1} alt="Jane" className="photo" />
+                <div className="container">
+                    <h2>Jane Doe</h2>
+                    <p className="title">CEO &amp; Founder</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                <img src={img2} alt="Mike" className="photo" />
+                <div className="container">
+                    <h2>Mike Ross</h2>
+                    <p className="title">Art Director</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="card">
+                <img src={img3} alt="John" className="photo" />
+                <div className="container">
+                    <h2>John Doe</h2>
+                    <p className="title">Designer</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p>example@example.com</p>
+                    <p><button className="buttonTeam">Contact</button></p>
+                </div>
+                </div>
+            </div>
+        </div>  
     </Layout>
   );
 }
