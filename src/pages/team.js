c0112d8de2544635bc7e81e560a860982525bced
Talgat Manglayev <talgatmanglayev@MacBook-Pro-Talgat.local>
2023-11-30 00:00:47 +0600

    added pages

---
+++ src/pages/team.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '@theme/Layout'
+
+function App()
+{
+  return(
+    <Layout>
+        <h1>My React page</h1>
+        <h2>REACT - The Complete Guide</h2>
+        <p>This is a React page</p>
+    </Layout>      
+  );
+}
+
+export default App;
